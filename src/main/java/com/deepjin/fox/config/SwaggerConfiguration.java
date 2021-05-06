ed30992c9b2767ee205194a5bdb8d3b4150f7573
C-YooJin <ioi_3312@naver.com>
2021-05-06 17:22:41 +0900

    Fix springfox version 2.9.2 -> 3.0.0

--- src/main/java/com/deepjin/fox/config/SwaggerConfiguration.java
+++ src/main/java/com/deepjin/fox/config/SwaggerConfiguration.java
@@ -8,13 +8,11 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfiguration {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("DeepJin Swagger project")
@@ -33,7 +31,7 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket swaggerApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
                 .consumes(getConsumeContentTypes())
                 .apiInfo(this.apiInfo()).select()
                 .apis(RequestHandlerSelectors.basePackage("com.deepjin.fox.controller"))
