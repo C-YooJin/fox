ac4a7aa0f73653b940835677069b88aa92334339
C-YooJin <ioi_3312@naver.com>
2021-05-06 10:44:24 +0900

    Add Controller and DAO

--- src/main/java/com/deepjin/fox/config/SwaggerConfiguration.java
+++ src/main/java/com/deepjin/fox/config/SwaggerConfiguration.java
@@ -10,19 +10,32 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("DeepJin Swagger project")
                 .description("Swagger UI를 활용한 rest api test 전용 페이지")
-                .license("Deepjin").licenseUrl("https://deep-jin.tistory.com/").version("1")
+                .license("Deepjin").licenseUrl("https://deep-jin.tistory.com/").version("1.0")
                 .build();
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=URF-8");
+        consumes.add("application/x-www-form-urlencoded");
+
+        return consumes;
     }
 
     @Bean
     public Docket swaggerApi() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(this.apiInfo()).select()
+        return new Docket(DocumentationType.SWAGGER_2)
+                .consumes(getConsumeContentTypes())
+                .apiInfo(this.apiInfo()).select()
                 .apis(RequestHandlerSelectors.basePackage("com.deepjin.fox.controller"))
                 .paths(PathSelectors.ant("/api/**"))
                 .build()
---
+++ src/main/java/com/deepjin/fox/controller/ApiController.java
@@ -0,0 +1,35 @@
+package com.deepjin.fox.controller;
+
+import com.deepjin.fox.dao.Member;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+@Api(tags = "back-front 간 rest api 테스트 ")
+public class ApiController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
+    ResponseEntity<?> entity = null;
+
+    @ApiOperation(value = "사용자 정보 ", tags = "tags 부분")
+    @GetMapping(value="/member")
+    public ResponseEntity<?> registerMember(String id, String name, String email) {
+        Member member = new Member();
+        member.setId(id);
+        member.setName(name);
+        member.setEmail(email);
+        entity = new ResponseEntity<Member>(member, HttpStatus.OK);
+
+        return entity;
+    }
+}
